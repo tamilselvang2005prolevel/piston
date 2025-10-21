c50d009777bb84323d530ff19cb1a31980c04ee7
TJprolevel <tamilselvang2005prolevel@gmail.com>
2025-10-21 17:58:21 +0530

    Fix paths for start.sh and Dockerfile

--- api/start.sh
+++ api/start.sh
@@ -3,6 +3,8 @@
 mkdir -p /tmp/isolate
 chmod 777 /tmp/isolate
 export PISTON_TEMPDIR=/tmp/isolate
+export DATA_DIRECTORY=/tmp/isolate
+export data_directory=/tmp/isolate
 
 # --- Start the main entrypoint ---
-exec /api/src/docker-entrypoint.sh
+exec node /api/src/index.js
