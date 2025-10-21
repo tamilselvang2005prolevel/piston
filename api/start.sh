f3ba8be278f393bb1c0fbeeb57408567ecbebe1b
TJprolevel <tamilselvang2005prolevel@gmail.com>
2025-10-21 17:20:36 +0530

    Fix paths for start.sh and Dockerfile

--- api/start.sh
+++ api/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# Make sure we have a writable directory for isolate
+# --- Render-safe writable setup ---
 mkdir -p /tmp/isolate
 chmod 777 /tmp/isolate
 export PISTON_TEMPDIR=/tmp/isolate
 
-# Start the regular entrypoint
-exec /piston_api/src/docker-entrypoint.sh
+# --- Start the main entrypoint ---
+exec /api/src/docker-entrypoint.sh
